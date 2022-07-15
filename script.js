78d967edc63fc8abfe5f7703c878c3f722cc205a
ricardo <ricardo.br.pi@gmail.com>
2022-07-15 20:27:31 -0300

    feat: create array and store input value

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function createTasks(event) {
-  event.preventDefault();
-  console.log('Hello');
+const input = document.querySelector('.input-value');
+const list = document.querySelector('.to-do-list');
+const taskArray = [];
+
+function checkValueTask() {
+  const valueInput = input.value.trim() != '';
+
+  return valueInput;
 }
 
-taskForm.addEventListener('submit', createTasks);
+function createTasks() {
+  const item = document.createElement('li');
+  const task = document.createElement('span');
+  const valueInput = input.value.trim();
+
+  list.appendChild(item);
+  item.appendChild(task);
+  task.innerHTML = valueInput;
+  addTaskArray();
+}
+
+function addTaskArray() {
+  const valueInput = input.value.trim();
+
+  taskArray.push(valueInput);
+  console.log(taskArray);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (checkValueTask()) {
+    createTasks();
+  }
+}
+
+taskForm.addEventListener('submit', handleSubmit);
