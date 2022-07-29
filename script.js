5a377f30d6c2c97bf90495411fd818097b34ebad
ricardo <ricardo.br.pi@gmail.com>
2022-07-28 22:55:19 -0300

    perf: change the way items were added to localStorage and displayed on screen

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 const input = document.querySelector('.input-value');
 const list = document.querySelector('.to-do-list');
-const taskArray = [];
+const taskArray = localStorage.getItem('task') || [];
+const idform = document.querySelector('#taskForm');
 
 function checkValueTask() {
   const valueInput = input.value.trim() != '';
 
   return valueInput;
 }
-
-function createTasks() {
-  const item = document.createElement('li');
-  const task = document.createElement('span');
+function createTask() {
   const valueInput = input.value.trim();
+  const savingTasks = JSON.parse(localStorage.getItem('task')) || [];
 
-  list.appendChild(item);
-  item.appendChild(task);
-  task.innerHTML = valueInput;
-  addTaskArray();
+  savingTasks.push(valueInput);
+
+  localStorage.setItem('task', JSON.stringify(savingTasks));
 }
 
-function addTaskArray() {
-  const valueInput = input.value.trim();
+function clearList() {
+  list.innerHTML = '';
+}
 
-  taskArray.push(valueInput);
-  console.log(taskArray);
+function generateList() {
+  const savingTasks = JSON.parse(localStorage.getItem('task')) || [];
+
+  for (i = 0; i < savingTasks.length; i++) {
+    const item = document.createElement('li');
+    const taskItem = document.createElement('span');
+
+    taskItem.innerHTML = savingTasks[i];
+    item.appendChild(taskItem);
+    list.appendChild(item);
+  }
+}
+
+function clearInput() {
+  input.value = '';
 }
 
 function handleSubmit(event) {
   event.preventDefault();
 
   if (checkValueTask()) {
-    createTasks();
+    createTask();
+    clearList();
+    generateList();
   }
+  clearInput();
 }
 
-taskForm.addEventListener('submit', handleSubmit);
+generateList();
+idform.addEventListener('submit', handleSubmit);
